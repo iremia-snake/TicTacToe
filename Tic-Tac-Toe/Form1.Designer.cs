58f84e261f9538967519da885d507d7124479ad3
Пётр <cherkashinpp@gmail.com>
2023-04-27 22:48:04 +0900

    Init

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.but1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Restart = new System.Windows.Forms.ToolStripMenuItem();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // but1
+            // 
+            this.but1.BackColor = System.Drawing.Color.PaleGreen;
+            this.but1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but1.FlatAppearance.BorderSize = 2;
+            this.but1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but1.Location = new System.Drawing.Point(12, 39);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(96, 96);
+            this.but1.TabIndex = 0;
+            this.but1.UseVisualStyleBackColor = false;
+            this.but1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightGreen;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.Restart});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(326, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.Help_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(126, 24);
+            this.Restart.Text = "Перезапустить";
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // but2
+            // 
+            this.but2.BackColor = System.Drawing.Color.PaleGreen;
+            this.but2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but2.FlatAppearance.BorderSize = 2;
+            this.but2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but2.Location = new System.Drawing.Point(114, 39);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(96, 96);
+            this.but2.TabIndex = 0;
+            this.but2.UseVisualStyleBackColor = false;
+            this.but2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but3
+            // 
+            this.but3.BackColor = System.Drawing.Color.PaleGreen;
+            this.but3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but3.FlatAppearance.BorderSize = 2;
+            this.but3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but3.Location = new System.Drawing.Point(216, 39);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(96, 96);
+            this.but3.TabIndex = 0;
+            this.but3.UseVisualStyleBackColor = false;
+            this.but3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but4
+            // 
+            this.but4.BackColor = System.Drawing.Color.PaleGreen;
+            this.but4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but4.FlatAppearance.BorderSize = 2;
+            this.but4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but4.Location = new System.Drawing.Point(12, 141);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(96, 96);
+            this.but4.TabIndex = 0;
+            this.but4.UseVisualStyleBackColor = false;
+            this.but4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but5
+            // 
+            this.but5.BackColor = System.Drawing.Color.PaleGreen;
+            this.but5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but5.FlatAppearance.BorderSize = 2;
+            this.but5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but5.Location = new System.Drawing.Point(114, 141);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(96, 96);
+            this.but5.TabIndex = 0;
+            this.but5.UseVisualStyleBackColor = false;
+            this.but5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but6
+            // 
+            this.but6.BackColor = System.Drawing.Color.PaleGreen;
+            this.but6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but6.FlatAppearance.BorderSize = 2;
+            this.but6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but6.Location = new System.Drawing.Point(216, 141);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(96, 96);
+            this.but6.TabIndex = 0;
+            this.but6.UseVisualStyleBackColor = false;
+            this.but6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but7
+            // 
+            this.but7.BackColor = System.Drawing.Color.PaleGreen;
+            this.but7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but7.FlatAppearance.BorderSize = 2;
+            this.but7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but7.Location = new System.Drawing.Point(12, 243);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(96, 96);
+            this.but7.TabIndex = 0;
+            this.but7.UseVisualStyleBackColor = false;
+            this.but7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but8
+            // 
+            this.but8.BackColor = System.Drawing.Color.PaleGreen;
+            this.but8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but8.FlatAppearance.BorderSize = 2;
+            this.but8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but8.Location = new System.Drawing.Point(114, 243);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(96, 96);
+            this.but8.TabIndex = 0;
+            this.but8.UseVisualStyleBackColor = false;
+            this.but8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but9
+            // 
+            this.but9.BackColor = System.Drawing.Color.PaleGreen;
+            this.but9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.but9.FlatAppearance.BorderSize = 2;
+            this.but9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but9.Location = new System.Drawing.Point(216, 243);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(96, 96);
+            this.but9.TabIndex = 0;
+            this.but9.UseVisualStyleBackColor = false;
+            this.but9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(326, 350);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Крестики-Нолики";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Restart;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but9;
+    }
+}
+
---
+++ Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool x_turn = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button senB = (Button)sender;
+            if (x_turn)
+            {
+                senB.Text= "X";
+            }
+            else
+            {
+                senB.Text= "O";
+            }
+            x_turn = !x_turn;
+            senB.Enabled = false;
+            CheckWin();
+        }
+        void CheckWin()
+        {
+            string[] but = { but1.Text, but2.Text, but3.Text, but4.Text, but5.Text, but6.Text, but7.Text, but8.Text, but9.Text, };
+            for (int i = 0; i < 3; i++)
+            {
+                int n = i * 3;
+                if (but[n] == but[n + 1] && but[n + 1] == but[n + 2] && but[n] != "")
+                {
+                    Win(but[n]);
+                }
+                if (but[i] == but[i + 3] && but[i + 3] == but[i + 6] && but[i] != "")
+                {
+                    Win(but[i]);
+                }
+            }
+            if (but[0] == but[4] && but[4] == but[8] && but[4] != "" || but[2] == but[4] && but[4] == but[6] && but[4] != "")
+            {
+                Win(but[4]);
+            }
+            bool draw = true;
+            foreach (string i in but)
+            {
+                if (i == "") { draw = false; break; }
+            }
+            if (draw) { Draw(""); }
+        }
+        void Win(string mes)
+        {
+            MessageBox.Show("выйграли " + mes);
+            Application.Restart();
+        }
+        void Draw(string mes)
+        {
+            MessageBox.Show("ничья" + mes);
+            Application.Restart();
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void Help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Игроки по очереди ставят на свободные клетки поля 3×3 знаки (один всегда крестики, другой всегда нолики). Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или диагонали, выигрывает. Первый ход делает игрок, ставящий крестики.",
+                "Справка"
+                );
+        }
+    }
+    
+}
